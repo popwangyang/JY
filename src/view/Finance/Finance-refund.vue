8ac2f57871066f0bbf5c954344042499a41e4ec0
wangzhaoguang <wangzhaoguang@hlchang.cn>
2019-03-08 15:26:07 +0800

    更新菜单列表同步原型图上

---
+++ src/view/Finance/Finance-refund.vue
@@ -0,0 +1,22 @@
+<!--
+    @Time    : 2019/3/4 12:58
+    @Author  : wangzhaoguang
+    @Email   : wangzhaoguang@hlchang.com
+    @File    : Finance-refund.vue
+    @Software: webstrom
+    @license : 娱网科道信息技术有限公司 copyright  2016-2019-->
+<template>
+    <div class="tmp">
+        退款管理
+    </div>
+</template>
+<script>
+export default {
+  components: {},
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+</style>
