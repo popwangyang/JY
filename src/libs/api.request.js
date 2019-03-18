67553e1cca8afa04036c4114e40c84615569720e
Your Name <wangyan@hlchang.cn>
2019-03-18 18:24:37 +0800

    保存

--- src/libs/api.request.js
+++ src/libs/api.request.js
@@ -1,6 +1,18 @@
 import HttpRequest from '@/libs/axios'
 import config from '@/config'
-const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
 
+var baseUrl = null;
+
+const url = window.location.href;
+    if(url.indexOf('info') > -1){
+        baseUrl = config.baseUrl.info;
+    }else if(url.indexOf('pre') > -1){
+        baseUrl = config.baseUrl.pre;
+    }else if(url.indexOf('test') > -1 || url.indexOf('localhost') > -1){
+        baseUrl = config.baseUrl.test;
+    }else{
+        baseUrl = config.baseUrl.dev;
+    }
+	
 const axios = new HttpRequest(baseUrl)
 export default axios
